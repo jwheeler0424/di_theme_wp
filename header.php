9437c7d85e1cb34e1e34cf84e647a66c7cf60250
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-10-19 15:54:52 -0700

    setup header and sass

--- header.php
+++ header.php
@@ -12,6 +12,8 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php bloginfo( 'name' ); wp_title(); ?></title>
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <?php if ( is_singular() && pings_open( get_queried_object() ) ): ?>
             <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
@@ -22,18 +24,17 @@
 <body <?php body_class(); ?>>
 
     <header class="header-container">
+        <div class="col-xs-12">
 
-        <div class="row">
-            <div class="col-xs-12">
-
-                <div class="container text-center">
-                    <div class="header-content">
+            <div class="container text-center">
+                <div class="header-content">
+                    <a href="#">
+                        <img class="site-logo" src="<?php header_image() ?>" alt="<?php echo alt_text_display(); ?>" />
                         <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-                    </div> <!-- .header-content -->
-                    <div class="nav-container"></div> <!-- .nav-container -->
-                </div> <!-- .header-container -->
+                    </a>
+                </div> <!-- .header-content -->
+                <div class="nav-container"></div> <!-- .nav-container -->
+            </div> <!-- .header-container -->
 
-            </div> <!-- .col-xs-12 -->
-        </div> <!-- .row -->
-
+        </div> <!-- .col-xs-12 -->
     </header>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -51,5 +51,20 @@ function di_load_scripts() {
     wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '5.1.3', true );
 
 }
-
 add_action( 'wp_enqueue_scripts', 'di_load_scripts' );
+
+/* Header Image Alt Text Display */
+function alt_text_display() {
+    
+    $data =  get_object_vars(get_theme_mod('header_image_data'));
+    $image_id = is_array($data) && isset($data['attachment_id']) ? $data['attachment_id'] : false;
+
+    if ($image_id) {
+
+        $image_alt = get_post_meta( $image_id, '_wp_attachment_image_alt', true);
+        return $image_alt;
+
+    }
+
+}
+add_action( 'wp_enqueue_scripts', 'alt_text_display' );
