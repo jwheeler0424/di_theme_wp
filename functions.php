95be09a54cd12fd989fd0f2258d8ab47a63ffb95
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-10-21 23:21:26 -0700

    changes

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require get_template_directory() . '/inc/cleanup.php';
-require get_template_directory() . '/inc/function-admin.php';
-require get_template_directory() . '/inc/function-front.php';
-require get_template_directory() . '/inc/enqueue.php';
-require get_template_directory() . '/inc/theme-support.php';
-require get_template_directory() . '/inc/custom-post-types/contact-message.php';
-require get_template_directory() . '/inc/walker.php';
