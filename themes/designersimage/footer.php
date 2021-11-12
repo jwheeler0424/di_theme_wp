434b4fae3b914203d642cd2a271a2ed505cbf0bf
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-11-11 16:19:45 -0800

    setup page footer

--- themes/designersimage/footer.php
+++ themes/designersimage/footer.php
@@ -6,7 +6,34 @@
  *  ##################################################
 */
 ?>
+        <footer class="di-footer__main">
 
-<?php wp_footer(); ?>
-</body>
+            <div class="di-footer__info">
+                <div class="container"></div>
+            </div>
+
+            <div class="di-footer__nav">
+                <div class="container">
+
+                    <div class="copyright">Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ); ?>" class="link-copyright"><?php bloginfo( 'name' ); ?></a>. All Rights Reserved.</div>
+
+                    <nav class="di-menu__footer" id="di-menu__footer">
+                        <?php
+                            $args = array(
+                                'theme_location' => 'footer',
+                                'container' => false,
+                                'menu_class' => 'di-menu__footer-nav',
+                                // 'walker' => new WalkerNavPrimary()
+                            );
+                            wp_nav_menu( $args );
+                        ?>
+                    </nav>
+
+                </div>
+            </div>
+
+        </footer>
+
+        <?php wp_footer(); ?>
+    </body>
 </html>
