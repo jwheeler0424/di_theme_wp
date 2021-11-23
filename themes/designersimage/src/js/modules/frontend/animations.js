c004a2a2d2a9575f282aa7b322d7489e29c63272
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-11-22 17:24:56 -0800

    fix css bug for testimonial slider and setup slider js

--- themes/designersimage/src/js/modules/frontend/animations.js
+++ themes/designersimage/src/js/modules/frontend/animations.js
@@ -11,9 +11,9 @@
 export const carousel = () => {
     const cards = document.querySelectorAll('.portfolio-carousel > .card'),
           cardsCount = cards.length,
-          buttonLeft = document.querySelector('button.left'),
-          buttonRight = document.querySelector('button.right'),
-          cardInfo = document.querySelector('.card-info');
+          buttonLeft = document.querySelector('.portfolio-carousel >button.left'),
+          buttonRight = document.querySelector('.portfolio-carousel >button.right'),
+          cardInfo = document.querySelector('.portfolio-carousel > .card-info');
     
     buttonLeft.addEventListener('click', () => {
         buttonLeft.disabled = true;
@@ -195,42 +195,8 @@ export const carousel = () => {
 
 export const slider = () => {
 
-    // global variables
-    const sliderView = document.querySelector('.di-slider--view > ul');
-    const sliderViewSlides = document.querySelectorAll('.di-slider--view__slides');
-    const arrowLeft = document.querySelector('.di-slider--arrows__left');
-    const arrowRight = document.querySelector('.di-slider--arrows__right');
-    const sliderLength = sliderViewSlides.length;
-
-    // sliding function
-    const slider = ( sliderViewItems, isActiveItem ) => {
-        // update the classes
-        isActiveItem.classList.remove('is-active');
-        sliderViewItems.classList.add('is-active');
-
-        // css transform the active slide position
-        sliderView.setAttribute('style', `transform:translateX(-${sliderViewItems.offsetLeft}px)`)
-    }
-
-    // before sliding function
-    const beforeSliding = (i) => {
-        let isActiveItem = document.querySelector('.di-slider--view__slides.is-active');
-        let currentItem = Array.from(sliderViewSlides).indexOf(isActiveItem) + i;
-        let nextItem = currentItem + i;
-        let sliderViewItems = document.querySelector(`.di-slider--view__slides:nth-child(${nextItem})`);
-
-        if (nextItem > sliderLength) {
-            sliderViewItems = document.querySelector('.di-slider--view__slides:nth-child(1)');
-        }
-
-        if (nextItem == 0) {
-            sliderViewItems = document.querySelector(`.di-slider--view__slides:nth-child(${sliderLength})`);
-        }
-
-        slider( sliderViewItems, isActiveItem );
-    }
-
-    // triggers arrows
-    arrowRight.addEventListener( 'click', () => beforeSliding(1) );
-    arrowLeft.addEventListener( 'click', () => beforeSliding(0) );
+    const leftArrow = document.querySelector('.testimonial-slider > button.left'),
+          rightArrow = document.querySelector('.testimonial-slider > button.right'),
+          cards = document.querySelectorAll('.testimonial-slider > figure'),
+          cardsCount = cards.length;
 }
