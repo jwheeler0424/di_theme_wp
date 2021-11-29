64b4cccfbb0dd055d54a8e679a50a3ab64e8b208
Jonathan Wheeler <jonathan.wheeler87@gmail.com>
2021-11-28 16:43:18 -0800

    completed contact page

--- themes/designersimage/searchform.php
+++ themes/designersimage/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" action="<?php echo home_url( '/' ); ?>" class="di-menu__search">
     <input type="search" class="form-control" placeholder="What can we help find?" value="<?php echo get_search_query() ?>" name="s" title="Search" />
-    <button type="submit" class="btn btn-search">
+    <button type="submit">
         <?php get_template_part( 'img/svg/icon', 'search.svg' ); ?>
     </button>
 </form>
